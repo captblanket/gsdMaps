23780cb0ce41609f6628ce8247705602a2add789
captblanket <captblanket@gmail.com>
2009-07-09 10:36:20 +0200

    now the main function starts when gmaps api has finished loading, container width and height can now be explicitly set

--- jquery.gsdmaps.js
+++ jquery.gsdmaps.js
@@ -1,39 +1,49 @@
 ;(function($) {
 
     // gsdMaps plugin removes 'Static' from Google Static Maps!
-
     $.fn.gsdMaps = function(options) {
-        var opts = $.extend(true, {}, $.fn.gsdMaps.defaults, options);
 
-        // extract the parameters from the first IMG encountered
-        var g = extractParams($(this).attr('src'));
+        var opts = $.extend(true, {}, $.fn.gsdMaps.defaults, options),
+            $self = this,
+            // extract parameters from the first <img> encountered;
+            // we need the API key
+            g = extractParams($self.attr('src'));
 
-        // if g.key is present, parameters have been successfully extracted
-        // therefore we can proceed
         if (g.key) {
+            // load the Google Maps JavaScript and call init
+            $.getScript('http://maps.google.com/maps?file=api&v=2&key='+g.key+'&async=2&callback=jQuery.fn.gsdMaps.init');
+        }
 
-            // load the Google Maps JavaScript
-            loadScript(g.key);
+        // Main function - create dynamic map from static
+        $.fn.gsdMaps.init = function() {
 
             // create our map-holding div
-            var $wrapper = $('<div />').attr(opts.wrapperAttrs).css(opts.wrapperCSS);
+            var $wrapper = $('<div />').attr(opts.wrapperAttrs);
 
-            return this.each(function() {
+            $self.each(function() {
                 var $this = $(this);
                 var $src = $this.attr('src');
                 $this.wrap($wrapper);
                 $this.css({'cursor' : 'pointer'});
 
                 $this.click(function() {
-                    $this.css({'opacity' : '0.3'});
+                
                     var gMapPars = parseParams($src);
-
+                    var mapEl = $this.parent();
+                    
+                    if (opts.wrapperCSS.width && opts.wrapperCSS.height) {
+                        mapEl.css(opts.wrapperCSS);
+                    } else {
+                        mapEl.css({'width' : gMapPars.width, 'height' : gMapPars.height});
+                    }
+                    $this.css({'opacity' : '0.3'});
+                    
                     // GMap2 constructor accepts only a DOM element, no jQuery sugar
-                    var mapEl = $this.parent().get(0);
-                    $(mapEl).css({'width' : gMapPars.width, 'height' : gMapPars.height});
+                    //var mapEl = $this.parent().get(0);
+                    //$(mapEl).css({'width' : gMapPars.width, 'height' : gMapPars.height});
 
                     // init map
-                    var map = new GMap2(mapEl);
+                    var map = new GMap2(mapEl.get(0));
                     map.setCenter(new GLatLng(gMapPars.latitude, gMapPars.longitude), gMapPars.zoom);
 
                     // init markers
@@ -47,24 +57,19 @@
                     var mapControl = new GMapTypeControl();
                     map.addControl(mapControl);
                     map.addControl(new GLargeMapControl3D());
+
                 });
+                $this.colorbox({inline:true, href:$this.parent()});
+
             });
 
-        }
+        };
 
         // private function for debugging
         function debug($obj) {
             if (window.console && window.console.log) {
                 window.console.log($obj);
             }
-        }
-
-        // this function loads Google Maps js
-        function loadScript(apiKey) {
-            var script = $('<script />');
-            script.attr('type', 'text/javascript');
-            script.attr('src', 'http://maps.google.com/maps?file=api&v=2&key='+apiKey+'&async=2');
-            script.appendTo('body');
         }
 
         // basic parameter extraction;
@@ -151,7 +156,13 @@
             return gMapParams;
         }
 
+        // this is the main return - a reference to the wrapping <div>
+        // now you can chain stuff...
+        //debug(this);
+        //return $self;
+
     };
+
 
     // default options
     $.fn.gsdMaps.defaults = {
@@ -163,8 +174,11 @@
             'position' : 'relative',
             'display'  : 'inline-block',
             '*display' : 'inline',
-            '*zoom'    : '1'
+            '*zoom'    : '1',
+            'width'    : 800,
+            'height'   : 600
         }
     };
+    
 
 })(jQuery);
